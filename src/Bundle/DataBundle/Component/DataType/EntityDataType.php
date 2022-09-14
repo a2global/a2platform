bbe44cbea5bd98e7a445bb4f3b32897d43c44e1f
alexander.advanced@gmail.com <alexanderadvanced>
2022-09-14 10:03:53 +0200

    Few great updates!

--- src/Bundle/DataBundle/Component/DataType/EntityDataType.php
+++ src/Bundle/DataBundle/Component/DataType/EntityDataType.php
@@ -6,16 +6,28 @@ use A2Global\A2Platform\Bundle\CoreBundle\Utility\StringUtility;
 
 class EntityDataType implements DataTypeInterface
 {
+    private const METHODS = [
+        'getName',
+        'getFullname',
+        'getTitle',
+    ];
+
     public static function getReadablePreview($value): string
     {
         if (method_exists($value, '__toString')) {
             return (string)$value;
-        } else {
-            return sprintf(
-                '%s #%s',
-                StringUtility::normalize(StringUtility::getShortClassName($value)),
-                $value->getId()
-            );
         }
+
+        foreach (self::METHODS as $method) {
+            if (method_exists($value, $method)) {
+                return (string)$value->$method();
+            }
+        }
+
+        return sprintf(
+            '%s #%s',
+            StringUtility::normalize(StringUtility::getShortClassName($value)),
+            $value->getId()
+        );
     }
 }
