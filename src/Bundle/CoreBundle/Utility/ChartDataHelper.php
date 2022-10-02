381c5ca09809872ebe6d865f913054a9006781fe
Alexander <alexander.advanced@gmail.com>
2022-10-02 10:46:07 +0200

    Few great updates!

--- src/Bundle/CoreBundle/Utility/ChartDataHelper.php
+++ src/Bundle/CoreBundle/Utility/ChartDataHelper.php
@@ -3,6 +3,7 @@
 namespace A2Global\A2Platform\Bundle\CoreBundle\Utility;
 
 use Exception;
+use DateTime;
 
 class ChartDataHelper
 {
@@ -27,7 +28,7 @@ class ChartDataHelper
         [50, 100, 50, 0.9],
     ];
 
-    public static function buildFromArray(array $data, $multiple = []): array
+    public static function buildFromArray(array $data, DateTime $from, DateTime $to, $multiple = []): array
     {
         $barNames = [];
 
@@ -39,16 +40,25 @@ class ChartDataHelper
         }
 
         // Set zero values
-        foreach ($data as $sectionName => $bars) {
+        $date = $from;
+        $filledWithZeroData = [];
+        $daysTotal = $from->diff($to)->days;
+
+        for ($i = 0; $i < $daysTotal; $i++) {
+            $day = $date->format('y-m-d');
+
             foreach ($barNames as $barName) {
-                $value = $data[$sectionName][$barName] ?? "0";
+                $value = $data[$day][$barName] ?? 0;
 
                 if ($multiple[$barName] ?? false && $value !== 0) {
                     $value *= (float)$multiple[$barName];
                 }
-                $data[$sectionName][$barName] = $value;
+                $filledWithZeroData[$day][$barName] = $value;
             }
+            $date->modify('+1 day');
         }
+        $data = $filledWithZeroData;
+
         $chartData = [
             'labels' => array_keys($data),
         ];
@@ -63,6 +73,9 @@ class ChartDataHelper
                     switch ($multiple[$barName]) {
                         case 0.001:
                             $label .= ' ×1k';
+                            break;
+                        case 1000:
+                            $label .= ' /1k';
                             break;
                         default:
                             throw new Exception('Unsupported multiplier');
