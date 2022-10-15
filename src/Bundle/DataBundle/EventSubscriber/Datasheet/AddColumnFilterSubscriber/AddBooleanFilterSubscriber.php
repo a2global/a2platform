c3f19871ab2807adaa0468fb5d5cdafc62a42668
Alexander <alexander.advanced@gmail.com>
2022-10-15 14:03:24 +0200

    Few great updates!

--- src/Bundle/DataBundle/EventSubscriber/Datasheet/AddColumnFilterSubscriber/AddBooleanFilterSubscriber.php
+++ src/Bundle/DataBundle/EventSubscriber/Datasheet/AddColumnFilterSubscriber/AddBooleanFilterSubscriber.php
@@ -2,17 +2,14 @@
 
 namespace A2Global\A2Platform\Bundle\DataBundle\EventSubscriber\Datasheet\AddColumnFilterSubscriber;
 
-use A2Global\A2Platform\Bundle\DataBundle\DataType\DateDataType;
-use A2Global\A2Platform\Bundle\DataBundle\DataType\DateTimeDataType;
-use A2Global\A2Platform\Bundle\DataBundle\DataType\FloatDataType;
-use A2Global\A2Platform\Bundle\DataBundle\DataType\IntegerDataType;
-use A2Global\A2Platform\Bundle\DataBundle\DataType\StringDataType;
+use A2Global\A2Platform\Bundle\DataBundle\DataType\BooleanDataType;
 use A2Global\A2Platform\Bundle\DataBundle\Event\Datasheet\OnDatasheetColumnFiltersBuildEvent;
+use A2Global\A2Platform\Bundle\DataBundle\Filter\BooleanDataFilter;
 use A2Global\A2Platform\Bundle\DataBundle\Filter\StringContainsDataFilter;
 use A2Global\A2Platform\Bundle\DataBundle\Manager\DatasheetParametersManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class AddStringContainsFilterSubscriber implements EventSubscriberInterface
+class AddBooleanFilterSubscriber implements EventSubscriberInterface
 {
     public function __construct(
         protected DatasheetParametersManager $parametersManager,
@@ -22,20 +19,16 @@ class AddStringContainsFilterSubscriber implements EventSubscriberInterface
     public function addFilter(OnDatasheetColumnFiltersBuildEvent $event)
     {
         if (!in_array(get_class($event->getColumn()->getType()), [
-            IntegerDataType::class,
-            StringDataType::class,
-            FloatDataType::class,
-            DateDataType::class,
-            DateTimeDataType::class,
+            BooleanDataType::class,
         ])) {
             return;
         }
         $parameters = $this->parametersManager->getDatasheetFilterParameters(
             $event->getDatasheet(),
-            StringContainsDataFilter::getName(),
+            BooleanDataFilter::getName(),
             $event->getColumn()->getName(),
         );
-        $filter = new StringContainsDataFilter();
+        $filter = new BooleanDataFilter();
         $this->parametersManager->applyParameters($filter, $parameters);
         $event->getDataReader()->addFieldFilter($event->getColumn()->getName(), $filter);
         $this->parametersManager->addFilterToDatasheetColumn(
---
+++ src/Bundle/DataBundle/FilterApplier/QueryBuilderBooleanFilterApplier.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace A2Global\A2Platform\Bundle\DataBundle\FilterApplier;
+
+use A2Global\A2Platform\Bundle\CoreBundle\Utility\QueryBuilderUtility;
+use A2Global\A2Platform\Bundle\DataBundle\Filter\BooleanDataFilter;
+use A2Global\A2Platform\Bundle\DataBundle\Filter\DataFilterInterface;
+use A2Global\A2Platform\Bundle\DataBundle\Reader\DataReaderInterface;
+use A2Global\A2Platform\Bundle\DataBundle\Reader\QueryBuilderDataReader;
+use Doctrine\ORM\QueryBuilder;
+
+class QueryBuilderBooleanFilterApplier implements FilterApplierInterface
+{
+    public function supports(DataReaderInterface $dataReader, DataFilterInterface $filter, string $fieldName = null): bool
+    {
+        return
+            $dataReader instanceof QueryBuilderDataReader
+            && $filter instanceof BooleanDataFilter
+            && !empty($fieldName);
+    }
+
+    public function apply(DataReaderInterface $dataReader, DataFilterInterface $filter, string $fieldName = null)
+    {
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = $dataReader->getSource();
+        $fieldPath = QueryBuilderUtility::getFieldPathByName($queryBuilder, $fieldName);
+
+        if ($filter->getValue() === '1') {
+            $queryBuilder->andWhere(sprintf('%s = 1', $fieldPath));
+        } else {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->orX(
+                    $queryBuilder->expr()->eq($fieldPath, 0),
+                    $queryBuilder->expr()->isNull($fieldPath),
+                )
+            );
+        }
+//        $queryBuilder
+//            ->andWhere(sprintf('%s LIKE :%sContains', $fieldPath, $fieldName))
+//            ->setParameter(sprintf('%sContains', $fieldName), sprintf('%%%s%%', $filter->getValue()));
+    }
+}
