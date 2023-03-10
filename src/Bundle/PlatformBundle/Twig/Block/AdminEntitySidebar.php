1fa1a5e710e3d729c3cd5ff22ba1095ce6e34582
Alexander <alexander.advanced@gmail.com>
2023-03-10 16:09:08 +0100

    Few great updates!

---
+++ src/Bundle/PlatformBundle/Twig/Block/AdminEntitySidebar.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace A2Global\A2Platform\Bundle\PlatformBundle\Twig\Block;
+
+class AdminEntitySidebar
+{
+    public const VIEW = 'admin_entity_view_sidebar';
+}
--- src/Bundle/PlatformBundle/Twig/Block/AdminTwigBlockContainer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace A2Global\A2Platform\Bundle\PlatformBundle\Twig\Block;
-
-class AdminTwigBlockContainer
-{
-    public const ENTITY_CRUD_VIEW = 'admin_entity_crud_view';
-}
