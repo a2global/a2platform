16129666194aabb5495a112765410ceaa9e2ee32
Alexander <alexander.advanced@gmail.com>
2023-03-11 08:57:04 +0100

    Few great updates!

--- src/Bundle/PlatformBundle/EventListener/Admin/AdminSidebarMenuBuildEventListener.php
+++ src/Bundle/PlatformBundle/EventListener/Admin/AdminSidebarMenuBuildEventListener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace A2Global\A2Platform\Bundle\PlatformBundle\EventListener\Admin;
 
 use A2Global\A2Platform\Bundle\PlatformBundle\Component\Menu\MenuItem;
+use A2Global\A2Platform\Bundle\PlatformBundle\Entity\WorkflowTransition;
 use A2Global\A2Platform\Bundle\PlatformBundle\Event\MenuBuildEvent;
 use A2Global\A2Platform\Bundle\PlatformBundle\Helper\EntityHelper;
 use A2Global\A2Platform\Bundle\PlatformBundle\Utility\StringUtility;
@@ -15,6 +16,10 @@ use Symfony\Component\Routing\RouterInterface;
 #[AsEventListener(event: 'a2platform.menu.build.admin_sidebar', method: 'postAdminSidebarMenuBuild', priority: -900)]
 class AdminSidebarMenuBuildEventListener
 {
+    const EXCLUDE_ENTITIES_LIST = [
+        WorkflowTransition::class,
+    ];
+
     public function __construct(
         protected RouterInterface $router,
         protected EntityHelper    $entityHelper,
@@ -38,9 +43,12 @@ class AdminSidebarMenuBuildEventListener
         sort($entityList);
 
         foreach ($entityList as $entityClassName) {
+            if (in_array($entityClassName, self::EXCLUDE_ENTITIES_LIST)) {
+                continue;
+            }
             $entityName = StringUtility::toSnakeCase($entityClassName);
             $menuItem = (new MenuItem('entity.' . $entityName))
-                ->setText('entity.name.single.'.StringUtility::toSnakeCase(StringUtility::getShortClassName($entityClassName)))
+                ->setText('entity.name.single.' . StringUtility::toSnakeCase(StringUtility::getShortClassName($entityClassName)))
                 ->setRouteName('admin_entity_index')
                 ->setRouteParameters([
                     'className' => $entityClassName,
